a1adf0dd6ecca95959d381a1a6d5eb1c747c79ae
Tom <thomas123mac@gmail.com>
2026-06-27 13:02:06 +1000

    change

--- Scripts/Util/MinigameManager.gd
+++ Scripts/Util/MinigameManager.gd
@@ -13,26 +13,29 @@ func start_minigame(duration: float = 30) -> void:
 	_timer.timeout.connect(_on_time_up)
 	
 func _on_time_up() -> void:
+	print("minigame timer up, emitting minigame_ended")
 	emit_signal("minigame_ended")
 
 func submit_score(steam_id: int, points: int) -> void:
+	print("submit_score: ", steam_id, " points: ", points)
 	received_scores[steam_id] = points
 	if received_scores.size() >= expected_players:
 		_calculate_results()
 
+
 func _calculate_results() -> void:
 	var results = []
-	
 	for steam_id in received_scores:
 		results.append({"steam_id": steam_id, "points": received_scores[steam_id]})
 	results.sort_custom(func(a, b): return a["points"] < b["points"])
+	print("results: ", results)
 	
 	var lose_count = results.size() / 2
 	for i in range(lose_count):
 		eliminate_player(results[i]["steam_id"])
 	
 	GameState.sync_and_finish()
-	
+	end_minigame()  # <-- was missing
 	
 	
 func end_minigame() -> void:
